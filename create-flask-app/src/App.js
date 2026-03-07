1701ff13c7910737017aaff4f0f2c138b0fedbc1
w3hackes <abhaydacharya@gmail.com>
2026-03-07 14:46:28 -0500

    e

--- create-flask-app/src/App.js
+++ create-flask-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> test</p>
+        <p> test 2</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
